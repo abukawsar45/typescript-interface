7ae23dbf77c5206a13ff04abc00c95ae8560cb4e
abukawsar45 <kawsar1146236@gmail.com>
2023-08-30 12:42:48 +0600

    class implementing interface

---
+++ class-implement/index.js
@@ -0,0 +1,11 @@
+// console.log('connected')
+var User = /** @class */ (function () {
+    function User(fullName, age) {
+        this.fullName = fullName;
+        this.age = age;
+    }
+    return User;
+}());
+;
+var user1 = new User('Abu Kaw Sar', 23);
+console.log(user1);
---
+++ class-implement/index.ts
@@ -0,0 +1,7 @@
+// console.log('connected')
+class User{
+  constructor(private fullName: string, private age:number){}
+};
+
+let user1 = new User('Abu Kaw Sar', 23);
+console.log(user1)
